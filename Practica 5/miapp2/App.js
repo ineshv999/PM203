464758d37542f43758c66228ade8fb185fdafe47
ineshv999 <123046059@upq.edu.mx>
2026-06-10 00:01:33 -0600

    Practica 6 Parte 2

--- Practica 5/miapp2/App.js
+++ Practica 5/miapp2/App.js
@@ -3,6 +3,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image} from 'react-native';
 import { Saludo } from './components/Saludo';
 import { Saludo2 } from './components/Saludo2';
+import { Perfil } from './components/Perfil';
 
 /* Zona 2: Main - Hogar de los componentes */
 export default function App() {
@@ -19,6 +20,9 @@ export default function App() {
       <Text>------------ Componente Propio Compuesto -----------</Text>
       <Saludo2></Saludo2>
 
+      <Text>------------ Componente Propio Perfil -----------</Text>
+      <Perfil></Perfil>
+      
       <StatusBar style="auto" />
 
     </View>
---
+++ Practica 5/miapp2/components/Perfil.js
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+
+export const Perfil=()=>{
+    return(
+        <View>
+            <Text>Nombre: Hernández Vicencio María Inés</Text>
+            <Text>Carrera: Ingeniería en Sistemas Computacionales</Text>
+            <Text>Materia: Programación Móvil</Text>
+            <Text>Cuatrimestre: Noveno cuatrimestre</Text>
+        </View>
+    )
+}
+
